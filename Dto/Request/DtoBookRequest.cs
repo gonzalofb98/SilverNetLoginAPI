30d49a52f86c5694703517e469452bdfed020bd1
Gonzalo <gonzalofb98@gmail.com>
2023-01-16 09:39:26 -0300

    Add Dtos for Book

---
+++ Dto/Request/DtoBookRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dto.Request
+{
+    public class DtoBookRequest
+    {
+        [Required]
+        [StringLength(500)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Author { get; set; }
+
+        public string Synopsis { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
---
+++ Dto/Response/DtoBookResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dto.Response
+{
+    public class DtoBookResponse
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Synopsis { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
